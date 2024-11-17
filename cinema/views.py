c95f454b3df5600e90efa8002adee12dd3c7b584
halyna-baklanova <novabalka@gmail.com>
2024-11-17 14:56:53 +0200

    add basic serializers, views and urls

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,35 @@
-# write views here
+from rest_framework import viewsets
+
+from cinema.models import Genre
+from cinema.serializers import (
+    GenreSerializer,
+    ActorSerializers,
+    CinemaHallSerializer,
+    MovieSerializer,
+    MovieSessionSerializer,
+)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = ActorSerializers
+
+
+class CinemaHallViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = MovieSerializer
+
+
+class MovieSessionViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = MovieSessionSerializer
